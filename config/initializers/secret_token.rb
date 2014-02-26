b183fe6ad944f7de1a30d0e881bc8f9f5325b809
Robby Ranshous <rranshous@gmail.com>
2014-02-26 18:11:14 -0500

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Landr::Application.config.secret_key_base = '5b53b92d608887ce542005009200be5b4a4082c86029e9b7d0b23aed423e80ac9812ec19cc4deaf1295fd092b28ea58ccd8774de681c70e1cef3c972423f28c9'
